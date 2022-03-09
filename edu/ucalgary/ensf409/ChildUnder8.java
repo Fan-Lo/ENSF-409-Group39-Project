1e0574ac01c5f81525da2da3263e98ec40ea32c4
Justin-kuhn <justin.kuhn2001@gmail.com>
2022-03-09 09:03:05 -0700

    Created AdultMale, AdultFemale, ChildOver8, ChildUnder8
    Created the 4 sub-classes of Person, as shown in the UML diagram.

---
+++ edu/ucalgary/ensf409/ChildUnder8.java
@@ -0,0 +1,21 @@
+/** 
+* ChildUnder8.java
+* @version 1.0
+* @since 1.0	
+* First draft of class ChildUnder8
+**/   
+
+package edu.ucalgary.ensf409;
+
+public class ChildUnder8 extends Person{
+	private static final Nutrition NUTRITIONAL_NEEDS = new
+	Nutrition(21, 33, 31, 15, 1400);
+	private final int CLIENT_ID = 4;
+	
+	public ChildUnder8(int age, char gender, boolean mobilityConcerns){
+		super(age, gender, mobilityConcerns);
+	}
+	public Nutrition getNutritionalNeeds(){
+		return NUTRITIONAL_NEEDS;
+	}
+}
