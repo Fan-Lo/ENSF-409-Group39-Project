4e77d87d9cbe5f77410b1a47fb4739f8d5b313b0
janpetallo <101074771+janpetallo@users.noreply.github.com>
2022-04-01 01:59:00 -0600

    Update FoodItem.java

--- edu/ucalgary/ensf409/FoodItem.java
+++ edu/ucalgary/ensf409/FoodItem.java
@@ -5,10 +5,22 @@ public class FoodItem {
     private final int ITEM_ID;
     private final Nutrition NUTRITION;
 
-    public FoodItem(String name, int ID, Nutrition nutrition) {
+    public FoodItem(int wholeGrain, int fruitsVeggies, int protein, int other, int calories, String name, int itemID){
         this.name = name;
-        this.ITEM_ID = ID;
-        this.NUTRITION = nutrition;
+        this.ITEM_ID = itemID;
+        this.NUTRITION = new Nutrition(wholeGrain, fruitsVeggies, protein, other, calories);
+    }
+
+    public int getItemID() {
+        return this.ITEM_ID;
+    }
+
+    public Nutrition getNutrition() {
+        return this.NUTRITION;
+    }
+
+    public String getName() {
+        return this.name;
     }
     
 }
